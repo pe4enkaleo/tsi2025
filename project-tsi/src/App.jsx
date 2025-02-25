3fb5609fad106d727d1f7bd929ce62372e283144
pe4enkaleo <pe4enkaleo@gmail.com>
2025-02-25 15:56:07 +0900

    update App.jsx
    починил переход по ссылкам

--- project-tsi/src/App.jsx
+++ project-tsi/src/App.jsx
@@ -3,22 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Header from './components/Header.jsx'
-import Home from './pages/Home.tsx'
-import About from './pages/About.tsx'
-import Blog from './pages/Blog.tsx'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import Blog from './pages/Blog'
 
 function App() {
   return (
     <>
-    <Header />
-    <BrowserRouter>
-      <Routes>
-        <Route to="/" element={<Home/>}/>
-        <Route to="/about" element={<About/>}/>
-        <Route to="/blog" element={<Blog/>}/>
-      </Routes>
-    </BrowserRouter>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/blog" element={<Blog/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
